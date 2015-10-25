bb122724830388f543ae19f00f31fbfc43eb723f
zenghis <zenghis@hotmail.com>
2015-10-25 13:30:04 +0800

    Create files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Shiny App
 <ul>
-<li>The Shiny app is a basic calculator that performs addition, subtraction, multiplication, and division.</li>
 <li>https://zenghis.shinyapps.io/Shiny</li>
+<li>The Shiny app is a basic calculator that performs addition, subtraction, multiplication, and division.</li>
 </ul>
--- ui.R
+++ ui.R
@@ -1,31 +1,29 @@
 library(shiny)
-shinyUI(
-  pageWithSidebar(
-    headerPanel("Basic Calculator"),
-    sidebarPanel(
-      numericInput("number1", label = h4("Number 1"), value = 0),
-      numericInput("number2", label = h4("Number 2"), value = 0)
-    ),
-    mainPanel(
-      tabsetPanel(type = "tabs", 
-        tabPanel("Documentation",
-          helpText('The Shiny app is a basic calculator that performs addition,
-                    subtraction, multiplication, and division.
-                    Types any two two numbers to get the answers,
-                    which shown in the Results tab.
-                    Sometimes, the display of the results could be slightly delayed,
-                    possibly due to heavy loads on the server.')
-        ), 
-        tabPanel("Results",
-          h4("Addition"),
-          verbatimTextOutput("add"),
-          h4("Subtraction"),
-          verbatimTextOutput("subtract"),
-          h4("Multiplication"),
-          verbatimTextOutput("multiply"),
-          h4("Division"),
-          verbatimTextOutput("divide"))
+shinyUI(fluidPage(
+    titlePanel("Basic Calculator"),
+    sidebarLayout(
+      sidebarPanel(
+        numericInput("number1", label = h4("Number 1"), value = 0),
+        numericInput("number2", label = h4("Number 2"), value = 0)),
+      mainPanel(
+        tabsetPanel(type = "tabs", 
+          tabPanel("Documentation",
+            helpText('The Shiny app is a basic calculator that performs addition,
+                      subtraction, multiplication, and division.
+                      Inserts any two numbers to get the answers,
+                      which shown in the Results tab.
+                      Sometimes, the display of the results could be slightly delayed,
+                      possibly due to heavy loads on the server.')), 
+          tabPanel("Results",
+            h4("Addition"),
+            verbatimTextOutput("add"),
+            h4("Subtraction"),
+            verbatimTextOutput("subtract"),
+            h4("Multiplication"),
+            verbatimTextOutput("multiply"),
+            h4("Division"),
+            verbatimTextOutput("divide"))
+        )
       )
     )
-  )
-)
+))
